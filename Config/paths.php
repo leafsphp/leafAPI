6c008a809cf396651e7f227e9382a8ef67973955
mr-phlames <mickdd22@gmail.com>
2020-06-19 01:13:35 +0000

    updated app paths

--- Config/paths.php
+++ Config/paths.php
@@ -1,41 +1,51 @@
 <?php
 
-function views_path($path = null) {
-	return "/app/views/$path";
+function views_path($path = null)
+{
+	return "App/Views/$path";
 };
 
-function storage_path($path = null) {
-	return "/storage/$path";
+function storage_path($path = null)
+{
+	return "storage/$path";
 };
 
-function controllers_path($path = null) {
-	return "/app/controllers/$path";
+function controllers_path($path = null)
+{
+	return "/App/Controllers/$path";
 }
 
-function models_path($path = null) {
-	return "/app/models/$path";
+function models_path($path = null)
+{
+	return "/App/Models/$path";
 };
 
-function migrations_path($path = null) {
-	return "/app/database/migrations/$path";
+function migrations_path($path = null)
+{
+	return "/App/Database/Migrations/$path";
 };
 
-function seeds_path($path = null) {
-	return "/app/database/seeds/$path";
+function seeds_path($path = null)
+{
+	return "/App/Database/Seeds/$path";
 };
 
-function factories_path($path = null) {
-	return "/app/database/factories/$path";
+function factories_path($path = null)
+{
+	return "/App/Database/Factories/$path";
 };
 
-function routes_path($path = null) {
-	return "/app/routes/$path";
+function routes_path($path = null)
+{
+	return "/App/Routes/$path";
 };
 
-function helpers_path($path = null) {
-	return "/app/helpers/$path";
+function helpers_path($path = null)
+{
+	return "/App/Helpers/$path";
 };
 
-function public_path($path = null) {
+function public_path($path = null)
+{
 	return "/public/$path";
 };
