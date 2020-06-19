c1edb972ec67575e6fd837b8688e0494e2266285
mr-phlames <mickdd22@gmail.com>
2020-06-19 03:08:47 +0000

    updated leaf console

--- Config/paths.php
+++ Config/paths.php
@@ -3,12 +3,12 @@
 function views_path($path = null)
 {
 	return "App/Views/$path";
-};
+}
 
 function storage_path($path = null)
 {
 	return "storage/$path";
-};
+}
 
 function controllers_path($path = null)
 {
@@ -18,34 +18,34 @@ function controllers_path($path = null)
 function models_path($path = null)
 {
 	return "/App/Models/$path";
-};
+}
 
 function migrations_path($path = null)
 {
 	return "/App/Database/Migrations/$path";
-};
+}
 
 function seeds_path($path = null)
 {
 	return "/App/Database/Seeds/$path";
-};
+}
 
 function factories_path($path = null)
 {
 	return "/App/Database/Factories/$path";
-};
+}
 
 function routes_path($path = null)
 {
 	return "/App/Routes/$path";
-};
+}
 
 function helpers_path($path = null)
 {
 	return "/App/Helpers/$path";
-};
+}
 
 function public_path($path = null)
 {
 	return "/public/$path";
-};
+}
