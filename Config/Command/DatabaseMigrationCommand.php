2767022193cbfd97e942ed845292fbc80c761b36
mr-phlames <mickdd22@gmail.com>
2020-11-17 18:07:13 +0000

    Added -f to db:migrate and db:install commmands

--- Config/Command/DatabaseMigrationCommand.php
+++ Config/Command/DatabaseMigrationCommand.php
@@ -5,48 +5,65 @@ namespace Config\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Illuminate\Support\Str;
 
 class DatabaseMigrationCommand extends Command
 {
     protected static $defaultName = "db:migrate";
-    
-    public function __construct() {
-        $this->migrationPath = dirname(dirname(__DIR__)) . '/App/Database/Migrations/';
+
+    public function __construct()
+    {
+        $this->migrationPath = dirname(dirname(__DIR__)) . migrations_path();
         parent::__construct();
     }
-    
-    protected function configure() {
+
+    protected function configure()
+    {
         $this
-        ->setDescription("Run the database migrations")
-        ->setHelp("Run the database migrations");
+            ->setDescription("Run the database migrations")
+            ->setHelp("Run the migrations defined in the migrations directory\n")
+            ->addOption('file', 'f', InputOption::VALUE_OPTIONAL, 'Rollback a particular file');
     }
-    
-    
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $this->_runMigrations($output);
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fileToRollback = $input->getOption('file');
+        $this->_runMigrations($output, $fileToRollback);
     }
-    
-    public function _runMigrations($output)
+
+    public function _runMigrations($output, $fileToRollback)
     {
         $migrations = glob($this->migrationPath . '*.php');
-        
+
         foreach ($migrations as $migration) {
             $file = pathinfo($migration);
             $filename = $file['filename'];
-                        
-            if ($filename !== "Schema"):
-                $className = '\App\Database\Migrations\\' . Str::studly(\substr($filename, 17));
-                $this->migrate($className, $filename);
 
-                $output->writeln('db migration on => ' . str_replace(dirname(dirname(__DIR__)) . '/App/Database/Migrations/', "", $migration));
+            if ($filename !== "Schema") :
+                $className = '\App\Database\Migrations\\' . Str::studly(\substr($filename, 17));
+
+                if ($fileToRollback) {
+                    if (strpos($migration, Str::snake("_create_$fileToRollback.php")) !== false) {
+                        $this->migrate($className, $filename);
+                        $output->writeln('<info>db migration on <comment>' . str_replace(dirname(dirname(__DIR__)) . migrations_path(), "", $migration . "</comment></info>"));
+                        exit();
+                    }
+
+                    continue;
+                } else {
+                    $this->migrate($className, $filename);
+                }
+
+                $output->writeln('<info>db migration on <comment>' . str_replace(dirname(dirname(__DIR__)) . migrations_path(), "", $migration . "</comment></info>"));
             endif;
         }
     }
 
-    protected function migrate($className, $filename) {
-        require_once "App/Database/Migrations/$filename.php";
+    protected function migrate($className, $filename)
+    {
+        require_once migrations_path("$filename.php", false);
 
         $class = new $className;
         $class->up();
--- Config/Command/DatabaseRollbackCommand.php
+++ Config/Command/DatabaseRollbackCommand.php
@@ -5,53 +5,77 @@ namespace Config\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Illuminate\Support\Str;
 
-class DatabaseRollbackCommand extends Command  {
-
+class DatabaseRollbackCommand extends Command
+{
     protected static $defaultName = "db:rollback";
+    protected $migrationPath;
 
     public function __construct()
     {
-        $this->migrationPath = dirname(dirname(__DIR__)) . '/App/Database/Migrations/';
         parent::__construct();
+        $this->migrationPath = dirname(dirname(__DIR__)) . migrations_path();
     }
 
     protected function configure()
     {
         $this
             ->setDescription("Rollback all database migrations")
-            ->setHelp("Rollback all database migrations")
-            ->addArgument('step', InputArgument::REQUIRED, 'step required');
+            ->setHelp("Rollback database migrations, add -f to rollback a specific file. Don't use -s and -f together\n")
+            ->addOption('step', 's', InputOption::VALUE_OPTIONAL, 'The batch to rollback')
+            ->addOption('file', 'f', InputOption::VALUE_OPTIONAL, 'Rollback a particular file');
     }
-
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->_runMigrations($input, $output);
+        $this->_rollback($input, $output);
     }
 
-    public function _runMigrations($input, $output)
+    protected function _rollback($input, $output)
     {
-        $migrations = glob($this->migrationPath . '*.php');
-        $step = $input->getArgument('step');
+        $migrations = glob("$this->migrationPath*.php");
 
-        if($step !== 'all'):
+        $step = $input->getOption('step');
+        $fileToRollback = $input->getOption('file');
+
+        if (!$step && !$fileToRollback) {
+            $output->writeln("<error>You need to specify a file or step to rollback</error>");
+            exit();
+        }
+
+        if ($step && $step !== 'all') {
             $migrations = array_slice($migrations, -abs($step), abs($step), true);
-        endif;
+        }
 
         foreach ($migrations as $migration) {
             $file = pathinfo($migration);
-            $filename = $file['filename'];
 
-            require_once $migration;
-            $className = 'App\Database\Migrations\\'.Str::studly(\substr($filename, 17));
-
-            $class = new $className;
-            $class->down();
-
-            $output->writeln('db rollback on => ' . str_replace(dirname(dirname(__DIR__)) . '/App/Database/Migrations/', "", $migration));
+            if ($fileToRollback) {
+                if (strpos($migration, Str::snake("_create_$fileToRollback.php")) !== false) {
+                    $this->_down($file, $migration, $output);
+                    exit();
+                }
+            } else {
+                $this->_down($file, $migration, $output);
+            }
         }
+
+        if ($fileToRollback && !in_array($fileToRollback, $migrations)) {
+            $output->writeln("<error>$fileToRollback not found!</error>");
+            exit();
+        }
+    }
+
+    protected function _down($file, $migration, $output)
+    {
+        require_once $migration;
+        $className = 'App\Database\Migrations\\' . Str::studly(\substr($file['filename'], 17));
+
+        $class = new $className;
+        $class->down();
+
+        $output->writeln("<info>db rollback on <comment>" . str_replace([dirname(dirname(__DIR__)) . migrations_path(), ".php"], "", $migration)) . "</comment></info>";
     }
 }
