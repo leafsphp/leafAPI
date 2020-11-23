76b7d4c51e859d9fc0bb1bd23dd84d72386e3602
mr-phlames <mickdd22@gmail.com>
2020-11-23 13:37:35 +0000

    :sparkles: finish up on factories

--- App/Database/Factories/Factory.php
+++ App/Database/Factories/Factory.php
@@ -12,100 +12,110 @@ use Illuminate\Support\Str;
  */
 abstract class Factory
 {
-    /**
-     * Faker class instance
-     */
-    public $faker;
-    
-    /**
-     * Generated factory data
-     */
-    protected $data;
+	/**
+	 * Faker class instance
+	 */
+	public $faker;
+	
+	/**
+	 * Generated factory data
+	 */
+	protected $data;
 
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    public $model = null;
+	/**
+	 * The name of the factory's corresponding model.
+	 *
+	 * @var string
+	 */
+	public $model = null;
 
-    public function __construct()
-    {
-        $this->faker = Faker::create();;
-    }
+	public function __construct()
+	{
+		$this->faker = Faker::create();;
+	}
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        //
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array
+	 */
+	public function definition()
+	{
+		//
+	}
 
-    /**
-     * Create a number of records based on definition
-     * 
-     * @param int $number The number of records to create
-     * 
-     * @return array
-     */
-    public function create(int $number)
-    {
-        $data = [];
+	/**
+	 * Create a number of records based on definition
+	 * 
+	 * @param int $number The number of records to create
+	 * 
+	 * @return self
+	 */
+	public function create(int $number)
+	{
+		$data = [];
 
-        for ($i=0; $i < $number; $i++) { 
-            $data[] = $this->definition();
-        }
+		for ($i=0; $i < $number; $i++) { 
+			$data[] = $this->definition();
+		}
 
-        $this->data = $data;
+		$this->data = $data;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Save created records in db
-     * 
-     * @param \App\Models\Model $model
-     * 
-     * @return true|Throwable
-     */
-    public function save($model = null, $useCreate = false)
-    {
-        $model = $model ?? $this->model ?? $this->getModelName();
-        
-        try {
-            foreach ($this->data as $item) {
-                if ($useCreate) {
-                    $model::create($item);
-                } else {
-                    $model = new $model;
-                    foreach ($item as $key => $value) {
-                        $model->{$key} = $value;
-                    }
-                    $model->save();
-                }
-            }
+	/**
+	 * Save created records in db
+	 * 
+	 * @param \App\Models\Model $model
+	 * 
+	 * @return true|Throwable
+	 */
+	public function save($model = null, $useCreate = false)
+	{
+		$model = $model ?? $this->model ?? $this->getModelName();
+		
+		try {
+			foreach ($this->data as $item) {
+				if ($useCreate) {
+					$model::create($item);
+				} else {
+					$model = new $model;
+					foreach ($item as $key => $value) {
+						$model->{$key} = $value;
+					}
+					$model->save();
+				}
+			}
 
-            return true;
-        } catch (\Throwable $th) {
-            return $th;
-        }
-    }
+			return true;
+		} catch (\Throwable $th) {
+			return $th;
+		}
+	}
 
-    /**
-     * Get the default model name
-     */
-    public function getModelName()
-    {
-        $class = get_class($this);
-        $modelClass = "\App\Models" . Str::studly(str_replace(["App\Database\Factories", "Factory"], "", $class));
-        
-        if (!class_exists($modelClass)) {
-            throw new \Exception("Couldn't retrieve model for " . get_class($this) . ". Add a \$model attribute to fix this.");
-        }
+	/**
+	 * Return created records
+	 * 
+	 * @return array
+	 */
+	public function get()
+	{
+		return $this->data;
+	}
 
-        return $modelClass;
-    }
+	/**
+	 * Get the default model name
+	 */
+	public function getModelName()
+	{
+		$class = get_class($this);
+		$modelClass = "\App\Models" . Str::studly(str_replace(["App\Database\Factories", "Factory"], "", $class));
+		
+		if (!class_exists($modelClass)) {
+			throw new \Exception("Couldn't retrieve model for " . get_class($this) . ". Add a \$model attribute to fix this.");
+		}
+
+		return $modelClass;
+	}
 }
--- App/Database/Seeds/UsersSeeder.php
+++ App/Database/Seeds/UsersSeeder.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Database\Seeds;
 
+use App\Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -13,16 +14,16 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+        // You can directly create db records
         // $user = new User();
         // $user->name = 'Mychi';
         // $user->email = "mickdd22@gmail.com";
         // $user->password = md5("password.demo");
         // $user->save();
 
-        // factory(App\User::class, 50)->create()->each(function ($user) {
-        //     $user->posts()->save(factory(App\Post::class)->make());
-        // });
+        // You can also use factories like this
+        (new UserFactory)->create(20)->save();
 
-        User::factory()->times(50)->create();
+        // even better, you can use them together :-)
     }
 }
