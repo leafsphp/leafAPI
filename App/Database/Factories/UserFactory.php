7df7fdc1034d26d92f185ef4b17b1c3663976792
mr-phlames <mickdd22@gmail.com>
2020-11-23 13:16:32 +0000

    :art: Updated default migrations

--- App/Database/Factories/UserFactory.php
+++ App/Database/Factories/UserFactory.php
@@ -9,20 +9,20 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * If this model property isn't defined, Leaf will
-     * try to generate the model name from the factory name
-     */
-    // public $model = User::class;
+	/**
+	 * If this model property isn't defined, Leaf will
+	 * try to generate the model name from the factory name
+	 */
+	// public $model = User::class;
 
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => Carbon::now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+	public function definition()
+	{
+		return [
+			'name' => $this->faker->name,
+			'email' => $this->faker->unique()->safeEmail,
+			'email_verified_at' => Carbon::now(),
+			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+			'remember_token' => Str::random(10),
+		];
+	}
 }
