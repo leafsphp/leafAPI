3834e276a0115edd841383979df047c0931a0f60
darko-mychi <mickdd22@gmail.com>
2020-12-14 17:23:43 +0000

    :recycle: Switched completely to aloe CLI

--- App/Console/ExampleCommand.php
+++ App/Console/ExampleCommand.php
@@ -9,8 +9,18 @@ class ExampleCommand extends Command
     public $description = "example command's description";
     public $help = "example command's help";
 
+    public function config()
+    {
+        // you can add arguments and options in the config method
+        $this
+            ->setArgument("argument", "required", "argument description")
+            ->setOption("option", "o", "required", "option description");
+    }
+
     public function handle()
     {
-        $this->comment("example's output");
+        $this->comment(
+            "example command's output {$this->argument('argument')} {$this->option('option')}"
+        );
     }
 }
