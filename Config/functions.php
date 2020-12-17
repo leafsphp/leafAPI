137f970421e710c0c0fb3e375e732ae73dc8d2e8
darko-mychi <mickdd22@gmail.com>
2020-12-17 02:06:47 +0000

    :fire: switched config to arrays

--- Config/functions.php
+++ Config/functions.php
@@ -226,3 +226,121 @@ if (!function_exists('view')) {
 		return app()->blade->render($view, $data, $mergeData);
 	}
 }
+
+// App paths as callable methods
+
+/**
+ * Get all app paths
+ */
+function app_paths($path = null, bool $slash = false)
+{
+	$paths = require __DIR__ . "/paths.php";
+	$res = !$path ? $paths : $paths[$path] ?? "/";
+	return $slash ? "/$res" : $res;
+}
+
+/**
+ * Views directory path
+ */
+function views_path($path = null, bool $slash = false)
+{
+	return app_paths("views_path", $slash) . "/$path";
+}
+
+/**
+ * Config directory path
+ */
+function config_path($path = null)
+{
+	return app_paths("config_path") . "/$path";
+}
+
+/**
+ * Storage directory path 
+ */
+function storage_path($path = null, bool $slash = false)
+{
+	return app_paths("storage_path", $slash) . "/$path";
+}
+
+/**
+ * Commands directory path
+ */
+function commands_path($path = null)
+{
+	return app_paths("commands_path") . "/$path";
+}
+
+/**
+ * Controllers directory path
+ */
+function controllers_path($path = null)
+{
+	return app_paths("controllers_path") . "/$path";
+}
+
+/**
+ * Models directory path
+ */
+function models_path($path = null)
+{
+	return app_paths("models_path") . "/$path";
+}
+
+/**
+ * Migrations directory path
+ */
+function migrations_path($path = null, bool $slash = true)
+{
+	return app_paths("migrations_path", $slash) . "/$path";
+}
+
+/**
+ * Seeds directory path
+ */
+function seeds_path($path = null)
+{
+	return app_paths("seeds_path") . "/$path";
+}
+
+/**
+ * Factories directory path
+ */
+function factories_path($path = null)
+{
+	return app_paths("factories_path") . "/$path";
+}
+
+/**
+ * Routes directory path
+ */
+function routes_path($path = null)
+{
+	return "/App/Routes/$path";
+	return app_paths("routes_path") . "/$path";
+}
+
+/**
+ * Helpers directory path
+ */
+function helpers_path($path = null)
+{
+	return "/App/Helpers/$path";
+	return app_paths("helpers_path") . "/$path";
+}
+
+/**
+ * Helpers directory path
+ */
+function lib_path($path = null)
+{
+	return app_paths("lib_path") . "/$path";
+}
+
+/**
+ * Public directory path
+ */
+function public_path($path = null)
+{
+	return app_paths("public_path") . "/$path";
+}
--- Config/paths.php
+++ Config/paths.php
@@ -1,105 +1,17 @@
 <?php
 
-/**
- * Views directory path
- */
-function views_path($path = null, bool $slash = false)
-{
-	return $slash ? "/App/Views/$path" : "App/Views/$path";
-}
-
-/**
- * Config directory path
- */
-function config_path($path = null)
-{
-	return "/Config/$path";
-}
-
-/**
- * Storage directory path 
- */
-function storage_path($path = null, bool $slash = false)
-{
-	return $slash ? "/storage/$path" : "storage/$path";
-}
-
-/**
- * Commands directory path
- */
-function commands_path($path = null)
-{
-	return "/App/Console/$path";
-}
-
-/**
- * Controllers directory path
- */
-function controllers_path($path = null)
-{
-	return "/App/Controllers/$path";
-}
-
-/**
- * Models directory path
- */
-function models_path($path = null)
-{
-	return "/App/Models/$path";
-}
-
-/**
- * Migrations directory path
- */
-function migrations_path($path = null, bool $slash = true)
-{
-	return  $slash ? "/App/Database/Migrations/$path" : "App/Database/Migrations/$path";
-}
-
-/**
- * Seeds directory path
- */
-function seeds_path($path = null)
-{
-	return "/App/Database/Seeds/$path";
-}
-
-/**
- * Factories directory path
- */
-function factories_path($path = null)
-{
-	return "/App/Database/Factories/$path";
-}
-
-/**
- * Routes directory path
- */
-function routes_path($path = null)
-{
-	return "/App/Routes/$path";
-}
-
-/**
- * Helpers directory path
- */
-function helpers_path($path = null)
-{
-	return "/App/Helpers/$path";
-}
-
-/**
- * Helpers directory path
- */
-function lib_path($path = null)
-{
-	return "/Lib/$path";
-}
-
-/**
- * Public directory path
- */
-function public_path($path = null)
-{
-	return "/public/$path";
-}
+return [
+	"controllers_path" => "/App/Controllers",
+	"models_path" => "/App/Models",
+	"migrations_path" => "/App/Database/Migrations",
+	"seeds_path" => "/App/Database/Seeds",
+	"factories_path" => "/App/Database/Factories",
+	"helpers_path" => "/App/Helpers",
+	"views_path" => "/App/Views",
+	"config_path" => "/App/Config",
+	"storage_path" => "/App/storage",
+	"commands_path" => "/App/Console",
+	"routes_path" => "/App/Routes",
+	"lib_path" => "/Lib",
+	"public_path" => "/public",
+];
