e22a27936ee1b2fd2c1cdba5a1035a56ba7fa3c1
mychi darko <mickdd22@gmail.com>
2021-04-30 17:50:39 +0000

    :alien: applied leaf v2.5 changes

--- index.php
+++ index.php
@@ -30,39 +30,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 | Require all Leaf API's Files
 |
 */
-require __DIR__. "/Config/bootstrap.php";
-
-/*
-|--------------------------------------------------------------------------
-| Initialise Leaf Core
-|--------------------------------------------------------------------------
-|
-| Plant a seed, grow the stem and return Leaf🤷‍
-|
-*/
-$app = new Leaf\App;
-
-/*
-|--------------------------------------------------------------------------
-| Default fix for CORS
-|--------------------------------------------------------------------------
-|
-| This just prevents the connection client from throwing
-| CORS errors at you.
-|
-*/
-$app->evadeCors(false);
-
-/*
-|--------------------------------------------------------------------------
-| Error Configuration
-|--------------------------------------------------------------------------
-|
-| Show or hide errors  for easy debugging
-| You might want to set this to false for production
-|
-*/
-Config\App::error_debug(true);
+require __DIR__ . "/Config/bootstrap.php";
 
 /*
 |--------------------------------------------------------------------------
@@ -78,14 +46,46 @@ require __DIR__ . "/Config/functions.php";
 
 /*
 |--------------------------------------------------------------------------
+| Attach blade view
+|--------------------------------------------------------------------------
+|
+| Since blade no longer ships with Leaf by default, we
+| can attach blade back to Leaf so you can use Leaf MVC
+| as you've always used it.
+|
+*/
+Leaf\View::attach(\Leaf\Blade::class);
+
+/*
+|--------------------------------------------------------------------------
+| Initialise Leaf Core
+|--------------------------------------------------------------------------
+|
+| Plant a seed, grow the stem and return Leaf🤷‍
+|
+*/
+$app = new Leaf\App(AppConfig());
+
+/*
+|--------------------------------------------------------------------------
+| Default fix for CORS
+|--------------------------------------------------------------------------
+|
+| This just prevents the connection client from throwing
+| CORS errors at you.
+|
+*/
+$app->evadeCors(false);
+
+/*
+|--------------------------------------------------------------------------
 | Route Config
 |--------------------------------------------------------------------------
 |
 | Require app routes.
 |
 */
-require __DIR__. "/App/Routes/index.php";
-
+require __DIR__ . "/App/Routes/index.php";
 
 /*
 |--------------------------------------------------------------------------
