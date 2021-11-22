5645f078d9c1dff55fa13b63fea5196b9e49eea4
Michael Darko <mickdd22@gmail.com>
2021-11-22 19:10:47 +0000

    :alien: updated example base controller

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -6,30 +6,30 @@ namespace App\Controllers;
 use Leaf\Auth;
 
 /**
- * This is the base controller for your Leaf API Project.
+ * This is the base controller for your Leaf MVC Project.
  * You can initialize packages or define methods here to use
  * them across all your other controllers which extend this one.
  */
-class Controller extends \Leaf\ApiController
+class Controller extends \Leaf\Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-		// In this version, request isn't initialised for you. You can use
-		// requestData() or request() to get request data or initialise it yourself
+        // In this version, request isn't initialised for you. You can use
+        // requestData() or request() to get request data or initialise it yourself
 
-		// autoConnect uses the .env variables to quickly connect to db
-		Auth::autoConnect();
+        // autoConnect uses the .env variables to quickly connect to db
+        Auth::autoConnect();
 
-		// set default token expiry time
-		Auth::tokenLifetime(60 * 60 * 24 * 365);
+        // You can configure auth to get additional customizations
+        // This can be done here with the Auth::config method or
+        // simply in the config/auth.php file
+        Auth::config(AuthConfig());
 
-		// You can configure auth to get additional customizations
-		// This can be done here with the Auth::config method or
-		// simply in the Config/auth.php file
-		Auth::config(AuthConfig());
+        // You can refer to https://leafphp.dev/modules/auth for auth docs
 
-		// You can refer to https://leafphp.netlify.app/#/leaf/v/2.5/core/auth for auth docs
-	}
+        // To use session instead of JWT, open up config/auth.php and set
+        // USE_SESSION to true
+    }
 }
