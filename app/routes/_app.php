4bde5fc68f045a1614ea5600b4a3ae8f528b812f
mychidarko <mickdd22@gmail.com>
2022-09-17 20:58:57 +0000

    :bug: fix blank screen for fresh install

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -1,10 +1,10 @@
 <?php
 
 app()->get("/", function () {
-    response(["message" => "Congrats!! You're on Leaf API"]);
+    response()->json(["message" => "Congrats!! You're on Leaf API"]);
 });
 
 app()->get("/app", function () {
     // app() returns $app
-    response(app()->routes());
+    response()->json(app()->routes());
 });
