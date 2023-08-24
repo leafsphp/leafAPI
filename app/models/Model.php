c8f7a03f108e28439f9b663dd30a1b2f4b2c55e0
mychidarko <mickdd22@gmail.com>
2023-08-24 19:25:22 +0000

    feat: update default models

--- app/models/Model.php
+++ app/models/Model.php
@@ -1,7 +1,13 @@
 <?php
+
 namespace App\Models;
 
 /**
  * Base Model
+ * ---
+ * The base model provides a space to set atrributes
+ * that are common to all models
  */
-class Model extends \Leaf\Model {}
+class Model extends \Leaf\Model
+{
+}
