6f2a7d377faaf987a57fa466402143dc66a72140
mychidarko <mickdd22@gmail.com>
2023-08-24 19:34:46 +0000

    feat: update cors config

--- config/cors.php
+++ config/cors.php
@@ -9,18 +9,18 @@ return [
     | Configures the Access-Control-Allow-Origin CORS header. Possible values:
     |
     | * String - set origin to a specific origin. For example if
-    |   you set it to 'http://example.com' only requests from
-    |   'http://example.com' will be allowed.
+    |   you set it to "http://example.com" only requests from
+    |   "http://example.com" will be allowed.
     |
     | * RegExp - set origin to a regular expression pattern which will be
     |   used to test the request origin. If it's a match, the request origin
     |   will be reflected. For example the pattern /example\.com$/ will reflect
-    |   any request that is coming from an origin ending with 'example.com'.
+    |   any request that is coming from an origin ending with "example.com".
     |
     | * Array - set origin to an array of valid origins. Each origin can be a String
-    |   or a RegExp. For example ['http://example1.com', /\.example2\.com$/] will
-    |   accept any request from 'http://example1.com' or from
-    |   a subdomain of 'example2.com'.
+    |   or a RegExp. For example ["http://example1.com", /\.example2\.com$/] will
+    |   accept any request from "http://example1.com" or from
+    |   a subdomain of "example2.com".
     |
     | * Function - set origin to a function implementing some custom
     |   logic. The function takes the request origin as the first parameter
