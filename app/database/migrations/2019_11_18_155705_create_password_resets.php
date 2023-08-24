5ddecdd45c865c4aab6127de981dec1b1c409cce
mychidarko <mickdd22@gmail.com>
2023-08-24 19:23:54 +0000

    feat: update default migrations

--- app/database/migrations/2019_11_18_155705_create_password_resets.php
+++ app/database/migrations/2019_11_18_155705_create_password_resets.php
@@ -1,18 +1,18 @@
 <?php
 
 use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreatePasswordResets extends Database
 {
     /**
      * Run the migrations.
-     *
      * @return void
      */
     public function up()
     {
-        if (!static::$capsule::schema()->hasTable("password_resets")):
-            static::$capsule::schema()->create("password_resets", function ($table) {
+        if (!static::$capsule::schema()->hasTable('password_resets')) :
+            static::$capsule::schema()->create('password_resets', function (Blueprint $table) {
                 $table->string('email')->index();
                 $table->string('token');
                 $table->timestamp('created_at')->nullable();
@@ -22,11 +22,10 @@ class CreatePasswordResets extends Database
 
     /**
      * Reverse the migrations.
-     *
      * @return void
      */
     public function down()
     {
-        static::$capsule::schema()->dropIfExists("password_resets");
+        static::$capsule::schema()->dropIfExists('password_resets');
     }
 }
