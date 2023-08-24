25dabb8e14f209b1a341c930a1be801da4b31cd9
mychidarko <mickdd22@gmail.com>
2023-08-24 18:16:19 +0000

    feat: update root file

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
 
-/**
- * Leaf API - A minimal but powerful MVC API framework
- *
- * @package  Leaf API
- * @author   Michael Darko <mychi.darko@gmail.com>
- */
+/*
+|--------------------------------------------------------------------------
+| Leaf API
+|--------------------------------------------------------------------------
+|
+| Leaf API is a minimal but powerful PHP MVC framework based on
+| the Leaf PHP framework for building APIs.
+|
+| This file allows us to run the app from the root of the project.
+| This provides a convenient way to test your Leaf API app
+| without having installed a "real" web server software here.
+|
+| It also allows you to directly load up your application from
+| the root file for quickly hosting on shared hosting platforms.
+|
+*/
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// This file allows us to run the app from the root of the project.
-// This provides a convenient way to test your Leaf MVC app
-// without having installed a "real" web server software here.
-
-// It also allows you to directly load up your application from
-// the root file for quickly hosting on shared hosting platforms.
 if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
     return false;
 }
