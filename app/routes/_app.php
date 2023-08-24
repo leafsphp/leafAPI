d28cb0c80ae24fc7734f954dbac8523f13c4fbcb
mychidarko <mickdd22@gmail.com>
2023-08-24 19:30:04 +0000

    feat: update routes to work with new modules

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -1,10 +1,5 @@
 <?php
 
-app()->get("/", function () {
-    response()->json(["message" => "Congrats!! You're on Leaf API"]);
-});
-
-app()->get("/app", function () {
-    // app() returns $app
-    response()->json(app()->routes());
+app()->get('/', function () {
+    response()->json(['message' => 'Congrats!! You\'re on Leaf API']);
 });
