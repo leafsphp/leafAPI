29b82b151baa0e7c577b8ab9c127d1ee9e10f007
mychidarko <mickdd22@gmail.com>
2023-08-24 19:29:54 +0000

    feat: update routes to work with new modules

--- app/routes/index.php
+++ app/routes/index.php
@@ -5,11 +5,13 @@
 | Set up 404 handler
 |--------------------------------------------------------------------------
 |
-| Create a handler for 404 errors
+| Leaf provides a default 404 page, but you can also create your
+| own 404 handler by calling app()->set404(). Whatever function
+| you set will be called when a 404 error is encountered
 |
 */
 app()->set404(function () {
-	response()->json("Resource not found", 404, true);
+	response()->json('Resource not found', 404, true);
 });
 
 /*
@@ -17,17 +19,13 @@ app()->set404(function () {
 | Set up 500 handler
 |--------------------------------------------------------------------------
 |
-| Create a handler for error 500
+| Leaf provides a default 500 page, but you can create your own error
+| 500 handler by calling the setErrorHandler() method. The function
+| you set will be called when a 500 error is encountered
 |
 */
-app()->setErrorHandler(function ($e = null) {
-    if ($e) {
-        if (app()->config("log.enabled")) {
-            app()->logger()->error($e);
-        }
-    }
-
-    response()->json("An error occured, our team has been notified", 500, true);
+app()->setErrorHandler(function () {
+    response()->json('An error occured, our team has been notified', 500, true);
 });
 
 /*
@@ -39,7 +37,20 @@ app()->setErrorHandler(function ($e = null) {
 | the controller namespace first.
 |
 */
-app()->setNamespace("\App\Controllers");
+app()->setNamespace('\App\Controllers');
 
-// You can break up routes into individual files
-require __DIR__ . "/_app.php";
+/*
+|--------------------------------------------------------------------------
+| Your application routes
+|--------------------------------------------------------------------------
+|
+| Leaf MVC automatically loads all files in the routes folder that
+| start with "_". We call these files route partials. An example
+| partial has been created for you.
+|
+| If you want to manually load routes, you can
+| create a file that doesn't start with "_" and manually require
+| it here like so:
+|
+*/
+// require __DIR__ . '/custom-route.php';
